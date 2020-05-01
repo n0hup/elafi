0b5f968be3b0a338df143dcc242ca00dec6f5b7b
Istvan Szukacs <istvan@lambdainsight.com>
2020-05-01 15:28:38 +0100

    Say my name

--- apps/dnscache/lib/dnscache/server.ex
+++ apps/dnscache/lib/dnscache/server.ex
@@ -1,20 +1,32 @@
 defmodule Dnscache.Server do
 
-	require Logger
 	use GenServer
+	require Logger
 
-  def start_link(_args) do
+  def start_link(args) do
 		Logger.info("Dnscache.Server is starting")
-		GenServer.start_link(__MODULE__, :ok, [])
+		Logger.info("Dnscache.Serverstart_link, args: #{inspect args}")
+		GenServer.start_link(__MODULE__, :ok)
   end
 
   def init(args) do
+		Logger.info("Dnscache.Server.init, args: #{inspect args}")
   	{:ok, args}
   end
 
-  def handle_call(message, _from, state) do
-    i = String.to_integer(message)
-    {:reply, i, state}
+	def get_my_state(pid) do
+		GenServer.call(pid, {:example})
+	end
+
+	def say_my_name() do
+		self() 
+	end
+
+	# Callbacks
+
+  def handle_call(message, from, state) do
+		Logger.info("Dnscache.Server.handle_call, args: #{inspect {message, from, state}}")
+		{:reply, state, state}
   end
 
 end
