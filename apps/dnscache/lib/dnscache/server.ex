8b64904ebc4bd5cb08fcf3bd3bb9e3cc452ecda0
Istvan <istvan@lambdainsight.com>
2020-05-02 23:20:24 +0200

    Crafting the response is tricky

--- apps/dnscache/lib/dnscache/server.ex
+++ apps/dnscache/lib/dnscache/server.ex
@@ -25,11 +25,15 @@ defmodule Dnscache.Server do
   end
 
   def handle_info(
-        {:udp, socket, source_ip, source_port,
-         <<id::16, qr::1, opcode::4, aa::1, tc::1, rd::1, ra::1, z::3, rcode::4,
-           qdcount::unsigned-integer-size(16), ancount::unsigned-integer-size(16),
-           nscount::unsigned-integer-size(16), arcount::unsigned-integer-size(16),
-           rest::binary>>},
+        {
+          :udp,
+          socket,
+          source_ip,
+          source_port,
+          <<id::16, qr::1, opcode::4, aa::1, tc::1, rd::1, ra::1, z::3, rcode::4,
+            qdcount::unsigned-integer-size(16), ancount::unsigned-integer-size(16),
+            nscount::unsigned-integer-size(16), arcount::unsigned-integer-size(16), rest::bits>>
+        },
         state
       ) do
     parsed_header =
@@ -37,10 +41,13 @@ defmodule Dnscache.Server do
        rcode, :qdcount, qdcount, :ancount, ancount, :nscount, nscount, :arcount, arcount}
 
     parsed_question = parse_dns_question(rest)
-
     Logger.info("Received DNS packet #{inspect(parsed_header)} #{inspect(parsed_question)}")
+    {:qname, qname, :qtype, qtype, :qclass, qclass} = parsed_question
 
-    :gen_udp.send(socket, source_ip, source_port, <<0::16>>)
+    response = create_response(id, qname, qtype, qclass)
+    Logger.info("Sending DNS packet #{inspect(response)}")
+
+    :gen_udp.send(socket, source_ip, source_port, response)
     {:noreply, state}
   end
 
@@ -50,47 +57,24 @@ defmodule Dnscache.Server do
     {:noreply, state}
   end
 
-  # defp create_response() do
-  #   <<id::16, qr::1, opcode::4, aa::1, tc::1, rd::1, ra::1, z::3, rcode::4, _rest::binary>>
-  # end
-
   defp parse_dns_question(bin) do
     parse_dns_question_acc({bin, {}, 1, true})
   end
-
-  # defp parse_dns_question_acc(
-  #        <<0::8, 0::8, qtype0::8, qtype1::8, qclass0::8, qclass1::8>>,
-  #        acc,
-  #        _l,
-  #        _f
-  #      ) do
-  #   Logger.info(
-  #     "Parsing DNS question(0) -> ret: #{inspect({qtype0, qtype1, qclass0, qclass1})} #{
-  #       inspect(acc)
-  #     }"
-  #   )
-
-  #   qtype = get_qtype({qtype0, qtype1})
-  #   qclass = get_qclass({qclass0, qclass1})
-  #   return = {:q, acc, :qtype, qtype, :qclass, qclass}
-  #   Logger.info("Parsing DNS question(0) -> ret: #{inspect(return)}")
-  #   return
-  # end
 
   defp parse_dns_question_acc(args) do
     Logger.info("Parsing DNS question(0) -> args: #{inspect(args)}")
 
     case args do
-      {<<0::8, 0::8, qtype0::8, qtype1::8, qclass0::8, qclass1::8, _rest::bits>>, acc, _, _} ->
-        qtype = get_qtype({qtype0, qtype1})
-        qclass = get_qclass({qclass0, qclass1})
-        return = {:q, acc, :qtype, qtype, :qclass, qclass}
+      {<<0::8, 0::8, qtype0::8, qtype1::8, qclass0::8, qclass1::8, rest::bits>>, acc, _, _} ->
+        # todo error handling?
+        {:ok, qtype} = qtype_to_string({qtype0, qtype1})
+        {:ok, qclass} = qclass_to_string({qclass0, qclass1})
+        return = {:qname, acc, :qtype, qtype, :qclass, qclass}
+        Logger.info("Parsing DNS question(return) -> rest: #{inspect(rest)}")
         Logger.info("Parsing DNS question(return) -> ret: #{inspect(return)}")
         return
 
-      {bin, acc, 1, true} ->
-        <<v::8, tail::bits>> = bin
-
+      {<<v::8, tail::bits>>, acc, 1, true} ->
         parse_dns_question_acc({
           tail,
           Tuple.append(acc, v),
@@ -116,24 +100,76 @@ defmodule Dnscache.Server do
     take_n_bits(rest, n - 1, acc <> <<v>>)
   end
 
-  defp get_qtype(qtype) do
+  defp create_response(id, qname, qtype, qclass) do
+    # THIS FUNCTION IS A DISASTER ZONE AT THIS STAGE
+    # MUST BE CLEANED UP AND SPLIT TO SMALLER CHUNKS
+    response_header =
+      <<id::16, 1::1, 0::4, 0::1, 0::1, 0::1, 0::1, 0::3, 0::4, 1::16, 1::16, 0::16, 0::16>>
+
+    #   id,     qr,   op,   aa,   tc,   rd,   ra,   z,    rc,   qdc,   anc,   nsc,   arc
+    name = get_name(qname)
+    {:ok, type} = string_to_qtype(qtype)
+    {:ok, class} = string_to_qclass(qclass)
+
+    # TODO: pls no
+    ttl = <<60::unsigned-integer-size(32)>>
+
+    response_answer =
+      name <>
+        :erlang.list_to_binary(Tuple.to_list(type)) <>
+        :erlang.list_to_binary(Tuple.to_list(class)) <>
+        ttl <> <<4::unsigned-integer-size(16)>> <> <<155::8, 33::8, 17::8, 68::8>>
+
+    response_header <> response_answer
+  end
+
+  defp qtype_to_string(qtype) do
     case qtype do
-      {1, 0} -> 'A'
-      {2, 0} -> 'NS'
-      {5, 0} -> 'CNAME'
-      {6, 0} -> 'SOA'
-      {12, 0} -> 'PTR'
-      {15, 0} -> 'MX'
-      {28, 0} -> 'AAAA'
-      {33, 0} -> 'SRV'
-      {255, 0} -> 'ANY'
+      {1, 0} -> {:ok, 'A'}
+      {2, 0} -> {:ok, 'NS'}
+      {5, 0} -> {:ok, 'CNAME'}
+      {6, 0} -> {:ok, 'SOA'}
+      {12, 0} -> {:ok, 'PTR'}
+      {15, 0} -> {:ok, 'MX'}
+      {28, 0} -> {:ok, 'AAAA'}
+      {33, 0} -> {:ok, 'SRV'}
+      {255, 0} -> {:ok, 'ANY'}
+      _ -> {:error, 'NONE'}
     end
   end
 
-  defp get_qclass(qclass) do
-    case qclass do
-      {1, 0} -> 'IN'
+  defp string_to_qtype(qtype) do
+    case qtype do
+      'A' -> {:ok, {1, 0}}
+      'NS' -> {:ok, {2, 0}}
+      'CNAME' -> {:ok, {5, 0}}
+      'SOA' -> {:ok, {6, 0}}
+      'PTR' -> {:ok, {12, 0}}
+      'MX' -> {:ok, {15, 0}}
+      'AAAA' -> {:ok, {28, 0}}
+      'SRV' -> {:ok, {33, 0}}
+      'ANY' -> {:ok, {255, 0}}
+      _ -> {:error, {0, 0}}
     end
+  end
+
+  defp qclass_to_string(qclass) do
+    case qclass do
+      {1, 0} -> {:ok, 'IN'}
+      _ -> {:error, 'NONE'}
+    end
+  end
+
+  defp string_to_qclass(qclass) do
+    case qclass do
+      'IN' -> {:ok, {1, 0}}
+      _ -> {:error, {0, 0}}
+    end
+  end
+
+  # TODO: convert qname to a list
+  defp get_name(qname) do
+    :erlang.list_to_binary(Tuple.to_list(qname))
   end
 
   def id_atom, do: __MODULE__
