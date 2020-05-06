717f2065609e58895e2fe05d1a86c11e50542e18
Istvan Szukacs <istvan@lambdainsight.com>
2020-05-06 14:33:54 +0100

    Adding udp_listen_ip

--- apps/dnscache/config/config.exs
+++ apps/dnscache/config/config.exs
@@ -1,6 +1,10 @@
 use Mix.Config
 
-# Application.get_env(:dnscache, :key)
-config :dnscache, udp_port_listen: 53
-config :mnesia, dir: 'elafidb_#{Mix.env}'
+config :dnscache, 
+	udp_port_listen: 53,
+	udp_ip_listen: {192,168,1,110}
+
+config :mnesia, 
+	dir: 'elafidb_#{Mix.env}'
+
 import_config "#{Mix.env()}.exs"
